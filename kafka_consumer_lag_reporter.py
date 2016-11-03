48bbe97d631ba7ed6887c58db780ab7aa4e3cc9b
Tuomas <tuomas@giosg.com>
2016-11-03 14:47:05 +0200

    Changed reporter to push values directly to Influxdb

--- kafka_consumer_lag_reporter.py
+++ kafka_consumer_lag_reporter.py
@@ -1,8 +1,11 @@
 import subprocess
 import argparse
+import datetime
+from influxdb import InfluxDBClient
+
 
 # VERSION 0.1
-# from https://github.com/paksu/kafka-consumer-lag-telegraf-reporter
+# from https://github.com/vainiot/kafka-consumer-lag-reporter
 
 OUTPUT_KEYS = ['group', 'topic', 'partition', 'current_offset', 'log_end_offset', 'lag']
 
@@ -29,12 +32,24 @@ def parse_output(input_from_checker):
     return output
 
 
-def to_line_protocol(parsed_output):
-    """
-    Converts the parsed output to InfluxDB line protocol metrics
-    """
-    return ["kafka.consumer_offset,topic={topic},group={group},partition={partition} current_offset={current_offset},log_end_offset={log_end_offset},lag={lag}"
-            .format(**line) for line in parsed_output]
+def to_influxdb_json(parsed_output):
+    json_body = [
+        {
+            "measurement": "kafka.consumer_offset",
+            "tags": {
+                "topic": line['topic'],
+                "group": line['group'],
+                "partition": line['partition']
+            },
+            "time": str(datetime.datetime.now()),
+            "fields": {
+                "current_offset": line['current_offset'],
+                "log_end_offset": line['log_end_offset'],
+                "lag": line['lag']
+            }
+        } for line in parsed_output
+    ]
+    return json_body
 
 
 def get_kafka(args):
@@ -67,10 +82,18 @@ if __name__ == "__main__":
     parser.add_argument('--group', help='Kafka group to check', required=True)
     parser.add_argument('--bootstrap-server', help='Which kafka to query. Used for new consumer')
     parser.add_argument('--zookeeper', help='Which zookeeper to query. Used for old consumer')
+    parser.add_argument('--idb_host', help='Influxdb host')
+    parser.add_argument('--idb_port', help='Influxdb port')
+    parser.add_argument('--idb_user', help='Influxdb user')
+    parser.add_argument('--idb_pass', help='Influxdb password')
+    parser.add_argument('--idb_db', help='Influxdb database')
     args = parser.parse_args()
     assert args.zookeeper or args.bootstrap_server, "requires either --zookeeper or --bootstarp-server"
+    assert args.idb_host and args.idb_port and args.idb_user and args.idb_pass and args.idb_db,\
+        "You must provide all influxdb parameters: idb_host, idb_port, idb_user, idb_pass, idb_db"
 
     output = get_kafka(args)
     parsed_output = parse_output(output)
-    for line in to_line_protocol(parsed_output):
-        print line
+    json_data = to_influxdb_json(parsed_output)
+    client = InfluxDBClient(args.idb_host, args.idb_port, args.idb_user, args.idb_pass, args.idb_db)
+    client.write_points(json_data)
